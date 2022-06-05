4c2c35182f70d9d95936da696bc0dad869180f4c
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-06-05 22:08:32 +0100

    admin

--- blog/resources/views/admin.blade.php
+++ blog/resources/views/admin.blade.php
@@ -14,18 +14,19 @@
     <a href="#home" class="w3-bar-item w3-button"><b>BR</b> admin</a>
     <!-- Float links to the right. Hide them on small screens -->
     <div class="w3-right w3-hide-small">
-      <a href="#projects" class="w3-bar-item w3-button">Projects</a>
+      <a href="#projects" class="w3-bar-item w3-button"></a>
       <a href="#about" class="w3-bar-item w3-button">About</a>
       <a href="#contact" class="w3-bar-item w3-button">Contact</a>
     </div>
   </div>
 </div>
 
+
 <!-- Header -->
 <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
   <img class="w3-image" src="/w3images/architect.jpg" alt="Architecture" width="1500" height="800">
   <div class="w3-display-middle w3-margin-top w3-center">
-    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span> <span class="w3-hide-small w3-text-light-grey">Architects</span></h1>
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span> <span class="w3-hide-small w3-text-light-grey"></span></h1>
   </div>
 </header>
 
@@ -40,13 +41,13 @@
   <div class="w3-row-padding">
     <div class="w3-col l3 m6 w3-margin-bottom">
       <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
+        <div class="w3-display-topleft w3-black w3-padding"> House</div>
         <img src="/w3images/house5.jpg" alt="House" style="width:100%">
       </div>
     </div>
     <div class="w3-col l3 m6 w3-margin-bottom">
       <div class="w3-display-container">
-        <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
+        <div class="w3-display-topleft w3-black w3-padding"> ll</div>
         <img src="/w3images/house2.jpg" alt="House" style="width:100%">
       </div>
     </div>
@@ -77,6 +78,11 @@
         <img src="/w3images/house5.jpg" alt="House" style="width:99%">
       </div>
     </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">House</div>
+        <img src="/w3images/house4.jpg" alt="House" style="width:99%">
+      </div>
     <div class="w3-col l3 m6 w3-margin-bottom">
       <div class="w3-display-container">
         <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
@@ -158,6 +164,8 @@
 <!-- Footer -->
 <footer class="w3-center w3-black w3-padding-16">
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+
+
 </footer>
 
 </body>
